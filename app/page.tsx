dd93c4e13eae7537dc9a2249948769f0dddd7ec6
Talalkhan <talalkhalidccr7@gmail.com>
2024-03-05 16:19:45 +0500

    Added Pricing.tsx
    Added Pricing component and stylized it, also fixed a few bugs

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Faq from "./components/Faq";
+import Pricing from "./components/Pricing";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <div className="px-[20px] lg:container lg:px-20 mx-auto">
         <Features /> 
         <Faq />
+        <Pricing />
       </div>
     </>
   );
